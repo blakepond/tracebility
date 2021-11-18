f265a3de6f713338c6b5a6595561e3a741d4057d
Blake Pond <blakehp@icloud.com>
2021-11-18 16:20:39 -0700

    typo fix

--- Server/index.js
+++ Server/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/", (req,res)=> {
-    res.sendFile(path.join(__dirname, "/Public/index.html"));
+    res.sendFile(path.join(__dirname, "../Public/index.html"));
     // rollbar.info("HTML file served successfully")
 })
 
